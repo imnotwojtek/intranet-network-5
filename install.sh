27c09ba4a464feab21868b8dbde5395dc350adf8
Wojtek <105372499+imnotwojtek@users.noreply.github.com>
2023-09-03 12:29:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Install Docker and Docker Compose
-sudo apt update
-sudo apt install -y docker.io docker-compose
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo apt update && sudo apt install -y docker.io docker-compose && sudo systemctl enable docker && sudo systemctl start docker
 
 # Create and decode Docker Compose file
 echo "dmVyc2lvbjogJzMnCnNlcnZpY2VzOgogIHdpcmVndWFyZDoKICAgIGltYWdlOiBsaW51eHNlcnZlci93aXJlZ3VhcmQKICAgIG5ldHdvcmtzOgogICAgICAtIG15X25ldHdvcmsKICBteXNxbDoKICAgIGltYWdlOiBteXNxbDpsYXRlc3QKICAgIG5ldHdvcmtzOgogICAgICAtIG15X25ldHdvcmsKICBtYWlsOgogICAgaW1hZ2U6IG1haWxzZXJ2ZXIvZG9ja2VyLW1haWxzZXJ2ZXI6bGF0ZXN0CiAgICBuZXR3b3JrczoKICAgICAgLSBteV9uZXR3b3JrCm5ldHdvcmtzOgogIG15X25ldHdvcms6CiAgICBkcml2ZXI6IGJyaWRnZQ==" | base64 -d > docker-compose.yml
@@ -19,8 +16,7 @@ for i in {1..8}; do
 done
 
 # Create email accounts
-docker exec -it mail setup email add hello@mail.wg mypassword
-docker exec -it mail setup email add no-reply@mail.wg mypassword
+docker exec -it mail setup email add hello@mail.wg mypassword & docker exec -it mail setup email add no-reply@mail.wg mypassword
 
 # Install Wiki.js
 docker run -d --name wiki --network my_network -e DB_TYPE=mysql -e DB_HOST=mysql -e DB_PORT=3306 -e DB_USER=root -e DB_PASS=root -e DB_NAME=wiki -p 3000:3000 requarks/wiki:2
