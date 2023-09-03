12b71601fd36f1bb5b129eeeecd4b662ce2fe45b
Wojtek <105372499+imnotwojtek@users.noreply.github.com>
2023-09-03 11:25:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,51 @@
 #!/bin/bash
 
-# Aktualizacja i instalacja Pi-hole
-sudo apt update -y && sudo apt upgrade -y && echo "yes" | sudo curl -sSL https://install.pi-hole.net | bash
+# Usunięcie Pi-hole i aktualizacja systemu
+pihole uninstall --unattended
+sudo apt update && sudo apt upgrade -y
 
-# Sprawdzenie i instalacja htop
-if ! command -v htop &> /dev/null; then
-    sudo apt install -y htop
-fi
+# Instalacja NGINX
+sudo apt install -y nginx
 
-# Instalacja iftop
-sudo apt install -y iftop
+# Instalacja i konfiguracja WireGuard
+sudo apt install -y wireguard
+wg genkey | sudo tee /etc/wireguard/privatekey | wg pubkey | sudo tee /etc/wireguard/publickey
+echo "[Interface]
+PrivateKey = $(sudo cat /etc/wireguard/privatekey)
+Address = 10.0.0.1/24
+ListenPort = 51820
+" | sudo tee /etc/wireguard/wg0.conf
+sudo systemctl enable wg-quick@wg0
+sudo systemctl start wg-quick@wg0
 
-# Tworzenie i konfiguracja 2GB pliku swap
-sudo fallocate -l 2G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+# Instalacja wg-dashboard
+sudo apt install -y git python3 python3-pip
+git clone https://github.com/Place1/wg-access-server.git
+cd wg-access-server
+./build.sh
+sudo ./wg-access-server --config ./config.yaml &
 
-# Zmiana serwera DNS
-echo -e "nameserver 1.1.1.1\nnameserver 1.0.0.1" | sudo tee /etc/resolv.conf > /dev/null
-
-# Restart usług
-sudo service networking restart
+# Konfiguracja NGINX dla stron wgx
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/CN=wgx"
+for site in help helpdesk data; do
+  echo "server {
+    listen 80;
+    server_name $site.wgx;
+    location / {
+      return 301 https://\$host\$request_uri;
+    }
+  }
+  server {
+    listen 443 ssl;
+    server_name $site.wgx;
+    ssl_certificate /etc/nginx/ssl/nginx.crt;
+    ssl_certificate_key /etc/nginx/ssl/nginx.key;
+    location / {
+      return 200 'hello $site';
+    }
+  }" | sudo tee /etc/nginx/sites-available/$site.wgx
+  sudo ln -s /etc/nginx/sites-available/$site.wgx /etc/nginx/sites-enabled/
+done
+sudo nginx -s reload
 
 echo "Zakończono konfigurację. Twój system jest teraz gotowy do użycia."
